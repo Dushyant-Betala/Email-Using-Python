b0c28185ae721487bade6374e5b43bf798f8bb10
Dushyant Betala <83596147+Dushyant-Betala@users.noreply.github.com>
2022-01-28 00:29:53 +0530

    Send Emails dynamically altered

---
+++ send specific email.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+
+html = Template(Path('Index.html').read_text())
+
+
+email =EmailMessage()
+email['from'] = 'Dushyant Betala'
+email['to'] = "db1624@srmist.edu.in"
+email['subject'] = 'Trial E-mail'
+
+email.set_content(html.substitute({'name':'TinTin', 'amount':'$7000'} ), 'html')
+
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('r.dushyantbetala2611@gmail.com', 'DushyantBetalaRock$26')
+    smtp.send_message(email)
+    print("All done!")  
